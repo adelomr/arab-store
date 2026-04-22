3499e789d962039222f5e61fe6b281005d5a5aae
Adel <adelomr1878@gmail.com>
2026-04-22 05:52:36 +0200

    إضافة هيكل Firebase App Check للمشروع

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,6 +3,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app-check.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAZetEdIaL6Y92PuWJXzkjZle5tNFMcwus",
@@ -20,4 +21,12 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export { app, db, auth, storage, analytics };
+// Initialize App Check
+// Note: You need to get a reCAPTCHA v3 site key from Firebase Console -> App Check
+// To enable debug mode, set self.FIREBASE_APPCHECK_DEBUG_TOKEN = true; before this
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6Ld_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'), // استبدل هذا بمفتاحك
+  isTokenAutoRefreshEnabled: true
+});
+
+export { app, db, auth, storage, analytics, appCheck };
